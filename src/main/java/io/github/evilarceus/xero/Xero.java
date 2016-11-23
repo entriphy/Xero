5eb957689cf07f46933f980093ddd0af1aca1dae
evilarceus <evilarceus@gmail.com>
2016-11-23 13:59:16 -0800

    Add inits

--- src/main/java/io/github/evilarceus/xero/Xero.java
+++ src/main/java/io/github/evilarceus/xero/Xero.java
@@ -1,8 +1,31 @@
 package io.github.evilarceus.xero;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="xero", name="Xero", version="0.1")
 public class Xero {
 
+    @Mod.Instance("Xero")
+    public static Xero instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
